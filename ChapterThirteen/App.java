703f5d959a9d2c80c611a8cbc217d7aff75e31cd
nwokoyepraise <nwokoyepraise@gmail.com>
2022-03-16 16:09:45 +0100

    solves excercise 29 of "Error Handling with Exceptions" chapter

--- ChapterThirteen/App.java
+++ ChapterThirteen/App.java
@@ -1,8 +1,5 @@
 import static util.Print.print;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-
 public class App {
 
     public static void main(String[] args) {
@@ -13,13 +10,6 @@ public class App {
         } catch (Exception e) {
             for (StackTraceElement ste : e.getStackTrace())
                 System.out.println(ste);
-        }
-
-        try {
-            BufferedReader br = new BufferedReader(new FileReader("Exercise1.java"));
-            
-        } catch (Exception e) {
-           
         }
     }
 }
---
+++ ChapterThirteen/Exercise29.java
@@ -0,0 +1,16 @@
+/**
+ * This class solves Exercise 29 Chapter 13 ["Error Handling with Exceptions"]
+ * of the
+ * book "Thinking in Java"
+ * This exercise shows that no exception spectifications or try blocks are
+ * necessary in StormyInning class when the subclasses extends from
+ * RuntimeException class
+ * 
+ * @author nwokoyepraise@gmail.com
+ */
+public class Exercise29 {
+
+    public static void main(String[] args) {
+        StormyInning.main(args);
+    }
+}
