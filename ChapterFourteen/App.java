46824409d11fef05f0cab48c582f999bb4499ef3
nwokoyepraise <nwokoyepraise@gmail.com>
2022-03-17 15:20:45 +0100

    solves excercise 2 of "Strings" - chapter 14

---
+++ ChapterFourteen/App.java
@@ -0,0 +1,10 @@
+// import java.util.*;
+
+public class App {
+
+    public static void main(String[] args) {
+        String mango = "mango";
+        String s = "abc" + mango + "def" + 47;
+        System.out.println(s);
+    }
+}
---
+++ ChapterFourteen/Exercise2.java
@@ -0,0 +1,14 @@
+
+/**
+ * This class solves Exercise 2 Chapter 14 ["Strings"] of the
+ * book "Thinking in Java"
+ * The class fixes unintended  recursion that would have otherwise occured in InfiniteRecursion.java
+ * 
+ * @author nwokoyepraise@gmail.com
+ */
+public class Exercise2 {
+    
+    public static void main(String[] args) {
+        InfiniteRecursion.main(args);
+    }
+}
