66cd374f0ecb2c14b5af9355869d5195b5ddf090
nwokoyepraise <nwokoyepraise@gmail.com>
2022-03-21 16:29:35 +0100

    change files structue

--- ChapterFifteen/Exercise1.java
+++ ChapterFifteen/Exercise1.java
@@ -1,69 +1,8 @@
-import static util.Print.print;
+import typeinfo.toys.*;
 
 public class Exercise1 {
 
     public static void main(String[] args) {
         ToyTest.main(args);
-    }
-}
-
-interface HasBatteries {
-}
-
-interface Waterproof {
-}
-
-interface Shoots {
-}
-
-class Toy {
-    // Comment out the following default constructor
-    // to see NoSuchMethodError from (*1*)
-    // Toy() {
-    // }
-
-    Toy(int i) {
-    }
-}
-
-class FancyToy extends Toy
-        implements HasBatteries, Waterproof, Shoots {
-    FancyToy() {
-        super(1);
-    }
-}
-
-class ToyTest {
-    static void printInfo(Class cc) {
-        print("Class name: " + cc.getName() +
-                " is interface? [" + cc.isInterface() + "]");
-        print("Simple name: " + cc.getSimpleName());
-        print("Canonical name : " + cc.getCanonicalName());
-    }
-
-    public static void main(String[] args) {
-        Class c = null;
-        try {
-            c = Class.forName("FancyToy");
-        } catch (ClassNotFoundException e) {
-            print("Can't find FancyToy");
-            System.exit(1);
-        }
-        printInfo(c);
-        for (Class face : c.getInterfaces())
-            printInfo(face);
-        Class up = c.getSuperclass();
-        Object obj = null;
-        try {
-            // Requires default constructor:
-            obj = up.newInstance();
-        } catch (InstantiationException e) {
-            print("Cannot instantiate");
-            System.exit(1);
-        } catch (IllegalAccessException e) {
-            print("Cannot access");
-            System.exit(1);
-        }
-        printInfo(obj.getClass());
     }
 }
