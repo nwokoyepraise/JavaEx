bd7e2e3163dbc18163f62e454c4bb83adb855d6e
nwokoyepraise <nwokoyepraise@gmail.com>
2022-04-06 18:55:36 +0100

    finalize chapter "Arrays"

--- ChapterSixteen/App.java
+++ ChapterSixteen/App.java
@@ -1,6 +1,50 @@
+import java.util.*;
+
+import static util.Print.print;
+
 public class App {
 
     public static void main(String[] args) {
-        
+
+    }
+}
+
+class Creature{}
+
+class Animal extends Creature {
+}
+
+class Cow extends Animal {
+}
+
+class Cat extends Cow {
+}
+
+class meow extends Cat {}
+
+class CheckedList {
+
+    public static void copy(List<? extends Number> source, List<? super Number> target) {
+        for (Number number : source) {
+            target.add(number);
+        }
+    }
+
+    public static void check(List<?> animals) {
+
+    }
+
+    public static void operate(List<? extends Number> input, List<? extends Object> anim, List<? super Cow> output) {
+        // one is only allowed to read f
+        for (Number number : input) {
+            // output.add(number);
+            output.add(new meow());
+            
+        }
+    }
+
+    public static void main(String[] args) {
+        List<Object> cows = new ArrayList<>();
+        check(cows);
     }
 }
