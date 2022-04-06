976cca463d7f261c7024946045bb9ca793413e12
nwokoyepraise <nwokoyepraise@gmail.com>
2022-04-06 18:09:01 +0100

    solves exercise 2 of "Arrays"

---
+++ ChapterSeventeen/Exercise2.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import static util.Print.print;
+
+/**
+ * This class solves Exercise 1 Chapter 17 ["Arrays"] of the
+ * book "Thinking in Java"
+ * 
+ * This exercise dmonstrates use of the Arrays.fill() method
+ * 
+ * @author @nwokoyepraise
+ */
+
+public class Exercise2 {
+    
+    static BerylliumSphere[] takesInt(int i) {
+        BerylliumSphere[] berylliumSpheres = new BerylliumSphere[i];
+        Arrays.fill(berylliumSpheres, new BerylliumSphere());
+        return berylliumSpheres;
+    }
+    public static void main(String[] args) {
+        print(Arrays.toString(takesInt(5)));
+    }
+}
